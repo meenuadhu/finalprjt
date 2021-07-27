5e2d025f571995399309e796b98043fad77da995
meenuadhu <meenuadhu@gmail.com>
2021-07-27 12:18:12 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,42 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { AllcoursesComponent } from './allcourses/allcourses.component';
+import { PartnershipComponent } from './partnership/partnership.component';
+import { MembershipComponent } from './membership/membership.component';
+import { EventsComponent } from './events/events.component';
+import { OfferingsComponent } from './offerings/offerings.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'aboutus',
+    component: AboutUsComponent
+  },
+   {
+    path: 'allcourses',
+    component: AllcoursesComponent
+  }, {
+    path: 'partnership',
+    component: PartnershipComponent
+  }, {
+    path: 'membership',
+    component: MembershipComponent
+  },
+  {
+    path:'offerings',
+    component:OfferingsComponent
+  },
+  {
+    path: 'events',
+    component: EventsComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { AllcoursesComponent } from './allcourses/allcourses.component';
+import { PartnershipComponent } from './partnership/partnership.component';
+import { MembershipComponent } from './membership/membership.component';
+import { EventsComponent } from './events/events.component';
+import { OfferingsComponent } from './offerings/offerings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    HeaderComponent,
+    AboutUsComponent,
+    AllcoursesComponent,
+    PartnershipComponent,
+    MembershipComponent,
+    EventsComponent,
+    OfferingsComponent
   ],
   imports: [
     BrowserModule,
